7561b71f5410946a5cac228dfadaf433093755ee
邵长久 <934125412@qq.com>
2019-10-15 20:52:02 +0800

    14

--- src/main/java/com/thread/ThreadPool3.java
+++ src/main/java/com/thread/ThreadPool3.java
@@ -19,6 +19,7 @@ public class ThreadPool3 {
                     System.out.println("delay 3 seconds");
                     System.out.println("delay 3 seconds");
                     System.out.println("delay 3 seconds");
+                    System.out.println("delay 3 seconds");
                 }
             }, 3, TimeUnit.SECONDS);
         }
