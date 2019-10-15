0077b6a799f642f35b6206a8e40804b439e1562d
邵长久 <934125412@qq.com>
2019-10-15 20:30:10 +0800

    7

--- src/main/java/com/thread/ThreadPool1.java
+++ src/main/java/com/thread/ThreadPool1.java
@@ -24,6 +24,7 @@ public class ThreadPool1 {
                         System.out.println(index);
                          System.out.println(index);
                         System.out.println(index);
+                        System.out.println(index);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
