1e8eff222ed6ec552ec38167a8c318ab86b68b7d
gitrebase <51101923+ChangJiuShao@users.noreply.github.com>
2019-10-15 20:35:10 +0800

    Update ThreadPool1.java

--- src/main/java/com/thread/ThreadPool1.java
+++ src/main/java/com/thread/ThreadPool1.java
@@ -25,6 +25,7 @@ public class ThreadPool1 {
                          System.out.println(index);
                         System.out.println(index);
                         System.out.println(index);
+                        System.out.println(index);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
