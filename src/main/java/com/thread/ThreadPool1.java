d69c4779a40f648d46f1ec2ae273a30352caf465
gitrebase <51101923+ChangJiuShao@users.noreply.github.com>
2019-10-15 20:54:05 +0800

    Update ThreadPool1.java

--- src/main/java/com/thread/ThreadPool1.java
+++ src/main/java/com/thread/ThreadPool1.java
@@ -30,6 +30,9 @@ public class ThreadPool1 {
                         System.out.println(index);
                         System.out.println(index);
                         System.out.println(index);
+                         System.out.println(index);
+                        System.out.println(index);
+                        System.out.println(index);
                         System.out.println(index);
                          System.out.println(index);
                         System.out.println(index);
